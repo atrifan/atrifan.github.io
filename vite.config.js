f1eb2a8cb0cd1d2b47960f2f66c9475734a4012f
Alex Trifan <trifan@adobe.com>
2026-01-01 14:13:50 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path for GitHub Pages (username.github.io/repo-name)
-  base: '/zip.run.place/',
+  // Base path for GitHub Pages (atrifan.github.io)
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
